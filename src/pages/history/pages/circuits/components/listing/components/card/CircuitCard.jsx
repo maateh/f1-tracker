3f432dbc5990f788f21f389fed26a619a5a47db3
maateh <mateharak01@gmail.com>
2023-09-13 12:28:58 +0200

    circuit cards styles

--- src/pages/history/pages/circuits/components/listing/components/card/CircuitCard.jsx
+++ src/pages/history/pages/circuits/components/listing/components/card/CircuitCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 // icons
 import LaunchIcon from '@mui/icons-material/Launch'
@@ -7,12 +7,19 @@ import LaunchIcon from '@mui/icons-material/Launch'
 import './CircuitCard.css'
 
 const CircuitCard = ({ circuit, lastRef }) => {
+  const navigate = useNavigate()
+
   return (
-    <li className="circuit-card__container" ref={lastRef ? lastRef : undefined}>
+    <li
+      className="circuit-card__container"
+      ref={lastRef || undefined}
+      onClick={() => navigate(`/profile/circuit/${circuit.id}`)}
+    >
       <h3 className="circuit-name">{circuit.name}</h3>
       <Link
         className="circuit-locality icon__container"
         to={circuit.getMapsLink()}
+        onClick={e => e.stopPropagation()}
       >
         <LaunchIcon fontSize='small' />
         <span>{circuit.location.country}, {circuit.location.locality}</span>
