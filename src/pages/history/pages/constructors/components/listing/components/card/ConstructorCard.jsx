34b559b2d1bdd1d6e06e3bae0de4490707497347
maateh <mateharak01@gmail.com>
2023-09-13 12:57:12 +0200

    ConstructorCard styles + functionality

--- src/pages/history/pages/constructors/components/listing/components/card/ConstructorCard.jsx
+++ src/pages/history/pages/constructors/components/listing/components/card/ConstructorCard.jsx
@@ -1,11 +1,35 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+// icons
+import PublicIcon from '@mui/icons-material/Public'
+import LaunchIcon from '@mui/icons-material/Launch'
+
 // styles
 import './ConstructorCard.css'
 
 const ConstructorCard = ({ constructor, lastRef }) => {
+  const navigate = useNavigate()
+
   return (
-    <li className="constructor-card__container" ref={lastRef ? lastRef : undefined}>
+    <li
+      className="constructor-card__container"
+      ref={lastRef || undefined}
+      onClick={() => navigate(`/profile/constructor/${constructor.id}`)}
+    >
       <h3 className="constructor-name">{constructor.name}</h3>
-      <p className="constructor-nationality">{constructor.nationality}</p>
+      <div className="constructor-nationality icon__container">
+        <PublicIcon fontSize='small' />
+        <span>{constructor.nationality}</span>
+      </div>
+      <Link
+        className="constructor-wiki__link icon__container"
+
+        to={constructor.wiki}
+        onClick={e => e.stopPropagation()}
+      >
+        <LaunchIcon fontSize='small' />
+        <span>Wiki link</span>
+      </Link>
     </li>
   )
 }
