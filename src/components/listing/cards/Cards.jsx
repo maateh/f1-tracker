d63e4daeb33958fb2ff55b52d9081f47f13fa572
maateh <mateharak01@gmail.com>
2023-10-13 11:00:29 +0200

    fixed searchParams, added searchOption to Cards

--- src/components/listing/cards/Cards.jsx
+++ src/components/listing/cards/Cards.jsx
@@ -1,23 +1,23 @@
 import { cloneElement } from 'react'
 
+// components
+import Search from '../../search/Search'
+
 // styles
 import './Cards.css'
 
-const Cards = ({ cards: { styles, layouts }, lastIndex, lastRef }) => {
-  
-  // TODO: items -> layouts[].props
-	// const filteredItems = useMemo(() => {
-	// 	return items.filter(item => item.toLowerCase().includes(query.toLowerCase))
-	// }, [items, query])
-
+const Cards = ({ cards: { styles, layouts }, searchOption, searchTitle, lastIndex, lastRef }) => {
   return (
-    <ul className="cards__container" style={styles}>
-      {layouts.map((layout, index) => {
-        return (index + 1) % lastIndex === 0
-          ? cloneElement(layout, { lastRef })
-          : layout
-      })}
-    </ul>
+    <>
+      {searchOption && <Search title={searchTitle} />}
+      <ul className="cards__container" style={styles}>
+        {layouts.map((layout, index) => {
+          return (index + 1) % lastIndex === 0
+            ? cloneElement(layout, { lastRef })
+            : layout
+        })}
+      </ul>
+    </>
   )
 }
 
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,14 +3,24 @@ import { useSearchParams } from 'react-router-dom'
 const Search = ({ title }) => {
 	const [searchParams, setSearchParams] = useSearchParams()
 
+  const handleChange = event => {
+    const query = event.target.value
+    if (query) {
+      setSearchParams({ q: event.target.value })
+      return
+    }
+    searchParams.delete('q')
+    setSearchParams(searchParams)
+  }
+
 	return (
 		<div className="search__container">
       <label>
         <span className="search-title">{title}</span>
 			  <input
           className="search-bar"
-          value={searchParams.get('q')}
-          onChange={e => setSearchParams({ q: e.target.value })}
+          value={searchParams.get('q') || ''}
+          onChange={handleChange}
           type="search"
         />
       </label>
